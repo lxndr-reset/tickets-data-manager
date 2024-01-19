44428c5b8097df3f79766ca87dc555cd20fbf144
lxndr <lxndr.nonstop@gmail.com>
2024-01-19 15:46:03 +0300

    Upgraded program logic
    Was mentioned the case when there's an empty list of tickets

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,23 +8,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String pathName;
+        String pathName = null;
 
-        System.out.println("Введите абсолютный путь до файла");
-        System.out.println("Пример: C:/users/user/tickets.json");
+        if (pathName == null) {
+            System.out.println("Введите абсолютный путь до файла");
+            System.out.println("Пример: C:/users/user/tickets.json");
 
-        Scanner scanner = new Scanner(System.in);
-        pathName = scanner.nextLine().trim().toLowerCase(Locale.ROOT);
-        scanner.close();
+            Scanner scanner = new Scanner(System.in);
+            pathName = scanner.nextLine().trim().toLowerCase(Locale.ROOT);
+            scanner.close();
+        }
 
         if (!pathName.endsWith("tickets.json")) {
             System.out.println("Неверный формат файла");
         }
         List<Ticket> tickets = new ParseTicketsFromFile(pathName).parse();
 
-        FastestFlightCalculator.calculateAndPrint(tickets);
-        System.out.println();
-        logMedianAndAverageDiff(tickets);
+        if (tickets.isEmpty()) {
+            System.out.println("There are no ticket objects in file " + pathName);
+        } else {
+
+            FastestFlightCalculator.calculateAndPrint(tickets);
+            System.out.println();
+            logMedianAndAverageDiff(tickets);
+        }
     }
 
     private static void logMedianAndAverageDiff(List<Ticket> tickets) {
@@ -37,8 +44,21 @@ public class Main {
         System.out.println("average: " + average);
         System.out.println("Difference between median and average: " + Math.abs(diff));
     }
+
     private static int getMedian(List<Ticket> tickets) {
-        return tickets.get(tickets.size() / 2).price();
+        int size = tickets.size(), res;
+
+        int dividedSize = size / 2;
+        if (size % 2 == 0) {
+            int leftPrice = tickets.get(dividedSize - 1).price();
+            int rightPrice = tickets.get(dividedSize).price();
+
+            res = (rightPrice + leftPrice) / 2;
+        } else {
+            res = tickets.get(dividedSize).price();
+        }
+
+        return res;
     }
 
     private static double getAverage(List<Ticket> tickets) {
